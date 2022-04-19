1c298bedc415820db782fcbb5b52d5d8f5d01eaf
jabesv <vjabes0@gmail.com>
2022-04-19 18:26:14 -0400

    first commit

---
+++ js lab/index.js
@@ -0,0 +1,51 @@
+console.log (typeof(15))
+// Prediction:
+// Actual: 
+
+console.log (typeof(5.5))
+// Prediction:
+// Actual: 
+
+console.log (typeof(NaN))
+// Prediction:
+// Actual: 
+
+console.log(typeof("hello"))
+// Prediction:
+// Actual: 
+
+console.log(typeof(true))
+// Prediction:
+// Actual: 
+
+console.log(typeof(1 != 2))
+// Prediction:
+// Actual: */ 
+
+console.log(typeof "Ostentatiou" + "s")
+// Prediction:
+// Actual:
+
+console.log(typeof "Facetious" - "s")
+// Prediction:
+// Actual:
+
+console.log(typeof("1" + "5"))
+// Prediction:
+// Actual:
+
+console.log(typeof("1" - "5"))
+// Prediction:
+// Actual:
+
+console.log(typeof("ivory" + "5"))
+// Prediction:
+// Actual:
+
+console.log(typeof("ivory" - "5"))
+// Prediction:
+// Actual:
+
+console.log(typeof(99 - "cheese rolling"))
+// Prediction:
+// Actual:*/
